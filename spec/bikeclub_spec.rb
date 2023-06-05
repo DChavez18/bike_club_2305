4c491fbf4f508d9fc8d124435a2fa41ecde66889
Derek Chavez <frrenose17@yahoo.com>
2023-06-05 11:52:04 -0600

    test: write tst for eligible bikers

--- spec/bikeclub_spec.rb
+++ spec/bikeclub_spec.rb
@@ -71,4 +71,29 @@ RSpec.describe BikeClub do
       expect(@bike_club_1.biker_with_best_time(ride2)).to eq(biker)
     end
   end
+
+  describe "#eligible" do
+    it "returns the bikers who are eligible based on terrain and max_distance" do
+      biker = Biker.new("Kenny", 30)
+      biker2 = Biker.new("Athena", 20)
+
+      @bike_club_1.add_biker(biker)
+      @bike_club_1.add_biker(biker2)
+
+      ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+      ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+
+      biker.learn_terrain!(:gravel)
+      biker2.learn_terrain!(:hills)
+      biker2.learn_terrain!(:gravel)
+
+      biker.log_ride(ride1, 90.5)
+      biker2.log_ride(ride1, 91.1)
+      biker.log_ride(ride2, 60.9)
+      biker2.log_ride(ride2, 61.6)
+      expect(@bike_club_1.eligible(ride1)).to eq(biker2)
+    end
+  end
+
+
 end
