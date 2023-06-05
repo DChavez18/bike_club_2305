afdf4bf4ea092e6957b007bae5300791da80419a
Derek Chavez <frrenose17@yahoo.com>
2023-06-05 11:11:15 -0600

    test: add add biker test

--- spec/bikeclub_spec.rb
+++ spec/bikeclub_spec.rb
@@ -5,12 +5,22 @@ require './lib/bikeclub'
 RSpec.describe BikeClub do
   before(:each) do
     @bike_club_1 = BikeClub.new("D's Bike Club")
+  end
+
   describe "#exists" do
     it "exists and has a name" do
       expect(@bike_club_1).to be_a(BikeClub)
-      expect(@bike_club_1).to eq("D's Bike Club")
+      expect(@bike_club_1.name).to eq("D's Bike Club")
     end
   end
-  
+
+  describe "#add_biker" do
+    it "can add a new biker to bikeclub" do
+      biker = Biker.new("Kenny", 30)
+      @bike_club_1.add_biker(biker)
+      expect(@bike_club_1.bikers).to eq(biker)
+    end
+  end
+
 
 end
