4ade7b12c513800d03cf071bcbb012ab715a8b41
Derek Chavez <frrenose17@yahoo.com>
2023-06-05 09:30:23 -0600

    test: write attributes test for biker

---
+++ spec/biker_spec.rb
@@ -0,0 +1,23 @@
+require './lib/ride'
+require './lib/biker'
+
+RSpec.describe Biker do
+  before(:each) do
+    @biker = Biker.new("Kenny", 30)
+    @biker2 = Biker.new("Athena", 15)
+    @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+    @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+  end
+
+  describe "#biker" do
+    it "has attributes and can learn new terrain" do
+      expect(@biker.name).to eq("Kenny")
+      expect(@biker.max_distance).to eq(30)
+      expect(@biker.rides).to eq({})
+      expect(@biker.acceptable_terrain).to eq([])
+      @biker.learn_terrain!(:gravel)
+      @biker.learn_terrain!(:hills)
+      expect(@biker.acceptable_terrain).to eq([:gravel, :hills])
+    end
+
+end
