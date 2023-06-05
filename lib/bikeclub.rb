8ff44d4ad5e6b0b2da382ed66113b5cafec12192
Derek Chavez <frrenose17@yahoo.com>
2023-06-05 11:27:13 -0600

    feat add biker with most rides method

--- lib/bikeclub.rb
+++ lib/bikeclub.rb
@@ -10,4 +10,19 @@ class BikeClub
     @bikers << biker
   end
 
+  def biker_with_most_rides
+    return nil if @bikers.empty?
+
+    most_rides_biker = @bikers.first
+    most_rides = most_rides_biker.rides_count
+
+    @bikers.each do |biker|
+      rides = biker.rides_count
+      if rides > most_rides
+        most_rides_biker = biker
+        most_rides = rides
+      end
+    end
+    most_rides_biker
+  end
 end
